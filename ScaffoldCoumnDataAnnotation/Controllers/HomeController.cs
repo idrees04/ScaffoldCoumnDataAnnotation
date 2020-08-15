046c866a95772ec741ec4c0faa55b38e79e76cf3
idrees04 <idreesar@yahoo.com>
2020-08-15 11:59:22 +0500

    Add project files.

---
+++ ScaffoldCoumnDataAnnotation/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScaffoldCoumnDataAnnotation.Controllers
+{
+    public class HomeController : Controller
+    {
+        //      SCAFFOLD COLUMN DATA ANNOTATION
+        //when you use scaffolding using the Create Templete,
+        //they view by default will create UI fields for all the properties of the model.
+        //However, you might need to ensure that the template does not create UI fields for certain properties.
+        //in such scenario, we will use the ScaffoldColumn annotation passing a false value.
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ScaffoldCoumnDataAnnotation/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ScaffoldCoumnDataAnnotation.Models
+{
+    public class Person
+    {
+        //SCAFFOLD COLUMN DATA ANNOTATION
+        //we  will use create templete view for all properties and we want to show just top three properties to user
+        //contact and address property will not autogenrate view if we use annotation [ScaffoldColumn(false)]
+        public string name { get; set; }
+        public string gender { get; set; }
+
+        public string age { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string contact { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string address { get; set; }
+
+    }
+}
